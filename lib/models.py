d02f96a59956bd8d4199df3929c4c6e718c9fa2e
M.K.Thiong'o <kamauthiongo58@gmail.com>
2025-12-03 18:27:54 +0300

    update freebie tracker

--- lib/models.py
+++ lib/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, create_engine
+from sqlalchemy.orm import relationship, backref, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 convention = {
@@ -9,6 +9,10 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+engine = create_engine('sqlite:///freebies.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
 class Company(Base):
     __tablename__ = 'companies'
 
@@ -16,8 +20,21 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
+    freebies = relationship('Freebie', backref=backref('company'))
+    devs = relationship('Dev', secondary='freebies', back_populates='companies')
+
     def __repr__(self):
         return f'<Company {self.name}>'
+
+    def give_freebie(self, dev, item_name, value):
+        freebie = Freebie(item_name=item_name, value=value, dev=dev, company=self)
+        session.add(freebie)
+        session.commit()
+        return freebie
+
+    @classmethod
+    def oldest_company(cls):
+        return session.query(cls).order_by(cls.founding_year).first()
 
 class Dev(Base):
     __tablename__ = 'devs'
@@ -25,5 +42,33 @@ class Dev(Base):
     id = Column(Integer(), primary_key=True)
     name= Column(String())
 
+    freebies = relationship('Freebie', backref=backref('dev'))
+    companies = relationship('Company', secondary='freebies', back_populates='devs')
+
     def __repr__(self):
         return f'<Dev {self.name}>'
+
+    def received_one(self, item_name):
+        return any(freebie.item_name == item_name for freebie in self.freebies)
+
+    def give_away(self, dev, freebie):
+        if freebie in self.freebies:
+            freebie.dev = dev
+            session.commit()
+            return True
+        return False
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+
+    def __repr__(self):
+        return f'<Freebie {self.item_name}>'
+
+    def print_details(self):
+        return f"{self.dev.name} owns a {self.item_name} from {self.company.name}"
